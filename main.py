6db14ae7d9565877d5818256867bb5ce2883d851
mark anthony dador <dador.markanthony@gmail.com>
2025-10-16 20:49:54 +0800

    add current_app/app.config

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ from taskdb import TaskDB
 from modify import modify
 from auth import auth
 
-taskdb = TaskDB()
 app = Flask(__name__)
+app.config['taskdb'] = TaskDB()
 
 app.register_blueprint(modify, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/')
 
 @app.route('/',methods=['POST','GET'])
 def home():
+    t_db = app.config['taskdb']
     if request.method == "POST":
         enter_task = request.form.get('content')
-        taskdb.add_list(enter_task)
+        t_db.add_list(enter_task)
         return redirect(url_for('home'))
     else:
-        view_list = taskdb.display_list()
+        view_list = t_db.display_list()
         return render_template('main.html',view_list=view_list)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
 
 
 '''
--- modify.py
+++ modify.py
@@ -1,20 +1,21 @@
-from flask import Blueprint,url_for,request,redirect,render_template
+from flask import Blueprint,url_for,request,redirect,render_template,current_app
 from taskdb import TaskDB
 
-taskdb = TaskDB()
 modify = Blueprint('modify',__name__)
 
 @modify.route('/delete/<int:no>')
 def del_route(no):
-    taskdb.del_list(no)
+    t_db = current_app.config['taskdb']
+    t_db.del_list(no)
     return redirect(url_for('home'))
 
 @modify.route('/update/<int:no>',methods=['POST','GET'])
 def update_route(no):
+    t_db = current_app.config['taskdb']
     if request.method == "POST":
         task_name = request.form.get('content')
-        taskdb.update_list(task_name,no)
+        t_db.update_list(task_name,no)
         return redirect(url_for('home'))
     else:
-        task_get = taskdb.get_list(no)
+        task_get = t_db.get_list(no)
         return render_template('update.html',task_get=task_get)
